f8e30d7ea2581e3ae7f2eb4b93e50228c3d12287
williamshdsilva <111313533+williamshdsilva@users.noreply.github.com>
2023-10-25 18:43:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Definindo as notas dos alunos
+var notaAluno1 = 8;
+var notaAluno2 = 7;
+var notaAluno3 = 9;
+
+// Calculando a média
+let media = (notaAluno1 + notaAluno2 + notaAluno3) / 3;
+
+// Exibindo o resultado
+console.log("A média das notas dos alunos é: " + media);
